74141b4198d0028255573db61487bf9254224ad0
Nathan Dorfman <ndorf@rtfm.net>
2020-07-12 18:48:26 -0600

    Accept arguments instead of hardcoding

--- ingest_DHT22.py
+++ ingest_DHT22.py
@@ -1,17 +1,47 @@
 #!/usr/bin/python3
 
 import Adafruit_DHT
+import argparse
 from influxdb import InfluxDBClient
 from datetime import datetime
 from socket import gethostname
 from sys import exit, stderr
 
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 27
+SENSOR_CHOICES = {
+    'DHT11': Adafruit_DHT.DHT11,
+    'DHT22': Adafruit_DHT.DHT22,
+    'AM2302': Adafruit_DHT.AM2302
+}
+
+args_parser = argparse.ArgumentParser(description='Ingest temperature/humidity sensor data into InfluxDB.')
+args_parser.add_argument('-P', '--pin',
+                         type=int,
+                         default=4,
+                         help='The GPIO pin the sensor is attached to. default: %(default)s')
+
+args_parser.add_argument('-S', '--sensor',
+                         choices=SENSOR_CHOICES.keys(),
+                         default="DHT22",
+                         help='The type of sensor. default: %(default)s')
+
+args_parser.add_argument('-D', '--database',
+                         default='sensors',
+                         help='The name of the InfluxDB database to write to. default: %(default)s')
+
+args_parser.add_argument('-M', '--measurement',
+                         default='temp',
+                         help='The name of the InfluxDB measurement to write to. default: %(default)s')
+
+args = args_parser.parse_args()
+
+DHT_SENSOR_NAME = args.sensor
+DHT_SENSOR = SENSOR_CHOICES[DHT_SENSOR_NAME]
+DHT_PIN = args.pin
 HOSTNAME = gethostname()
+MEASUREMENT = args.measurement
 
 client = InfluxDBClient()
-client.switch_database('temps')
+client.switch_database(args.database)
 
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
@@ -19,7 +49,7 @@ while True:
     if humidity is not None and temperature is not None:
         if True != client.write_points([
             {
-                "measurement": "temp",
+                "measurement": MEASUREMENT,
                 "time": datetime.now().astimezone().isoformat(),
                 "fields": {
                         "temperature": temperature,
@@ -27,7 +57,7 @@ while True:
                 },
                 "tags": {
                         "host": HOSTNAME,
-                        "sensor": "DHT22"
+                        "sensor": DHT_SENSOR_NAME
                 }
             }
         ]):
